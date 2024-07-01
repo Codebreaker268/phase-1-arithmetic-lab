aacc4a7ecb01ae288ee30e2861658f6828a077cc
Godfrey Onyango <godiodhis268@gmail.com>
2024-07-01 19:29:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let random=Math.floor((Math.random()+1) * 10);
+
+console.log(random);
+
+let max=Math.max(2,4,13,20,12);
+console.log(max);
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+console.log(multiply);
+let num3=32;
+let num4=7;
+let mod=num3%num4;
+console.log(mod);
